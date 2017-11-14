2ca07eabd6662ec8dacfbe65d0863da4b01b4779
Dane Filipczak <dane@imaginationrock.com>
2017-11-14 13:41:15 -0500

    trying to fix client secrets bug

--- bodytalk/app.py
+++ bodytalk/app.py
@@ -111,7 +111,7 @@ def gconnect():
         try:
             # Upgrade the authorization code into a credentials object
             oauth_flow = flow_from_clientsecrets(
-                'client_secrets.json', scope='')
+                dir_path + '/' + 'client_secrets.json', scope='')
             oauth_flow.redirect_uri = 'postmessage'
             credentials = oauth_flow.step2_exchange(code)
         except FlowExchangeError:
